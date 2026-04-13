2364120cbad2f91b5b77d975a3fd6fc2c007bfd2
Kit <kit@Kits-MacBook-Pro.local>
2026-04-13 19:23:40 +0100

    Fixed css import errors

--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -1,39 +1,39 @@
 <script lang="ts">
-	import { unmount } from "svelte";
-	import { myPosts } from "./Feed.svelte";
-	import { logOut } from "./request.ts";
-	import SignInPopup from "./SignInPopup.svelte";
-	import User, { clearUser } from "./User.svelte";
+    import { unmount } from "svelte";
+    import { myPosts } from "./Feed.svelte";
+    import { logOut } from "./request.ts";
+    import SignInPopup from "./SignInPopup.svelte";
+    import User, { clearUser } from "./User.svelte";
 
-	let visible = $state(false); // is the logout message visible
+    let visible = $state(false); // is the logout message visible
 
-	async function handleLogOut() {
-		const result = await logOut(); //make the request
-		console.log(`Logging Out: ${result}`);
+    async function handleLogOut() {
+        const result = await logOut(); //make the request
+        console.log(`Logging Out: ${result}`);
 
-		//clear user
-		clearUser();
-		// show that the user has logged out for 3 seconds
-		visible = true;
-		setTimeout(() => {
-			visible = false;
-		}, 3000);
-	}
+        //clear user
+        clearUser();
+        // show that the user has logged out for 3 seconds
+        visible = true;
+        setTimeout(() => {
+            visible = false;
+        }, 3000);
+    }
 </script>
 
 <!-- The Sidebar -->
 <div class="sidebar">
-	<User />
-	<SignInPopup />
+    <User />
+    <SignInPopup />
 
-	<button id="logout" onclick={handleLogOut} class="button"> LogOut </button>
-	{#if visible}
-		<p style="color: red; background-color: darkslategrey;">
-			You have been logged out
-		</p>
-	{/if}
+    <button id="logout" onclick={handleLogOut} class="button"> LogOut </button>
+    {#if visible}
+        <p style="color: red; background-color: darkslategrey;">
+            You have been logged out
+        </p>
+    {/if}
 </div>
 
 <style>
-	@import "./src/app.css";
+    @import "./app.css";
 </style>
